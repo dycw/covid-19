a620f7ecc319799d42e9f30baad5c42763864fd1
Derek Wan <d.wan@icloud.com>
2022-02-13 11:36:19 +0800

    Update (#2)
    * Add
    
    * generate
    
    * Update plot
    
    * update

--- src/covid_19/generate.py
+++ src/covid_19/generate.py
@@ -4,14 +4,22 @@ from pathlib import Path
 from typing import cast
 
 import holoviews.plotting.bokeh  # type: ignore # noqa: F401
+from click import DateTime
+from click import command
+from click import option
 from holoviews import save
 
 from covid_19.covid_19 import get_combined_plot
 
 
-def main() -> None:
+@command()
+@option(
+    "-s", "--start", type=DateTime(), default="2021-12-01", show_default=True
+)
+@option("--smooth", type=int, default=3, show_default=True)
+def main(start: str, smooth: int) -> None:
     countries_and_scales = [("HKG", None), ("JPN", 3), ("USA", 3), ("GBR", 3)]
-    plot = get_combined_plot(countries_and_scales, start="2021-12-01", smooth=3)
+    plot = get_combined_plot(countries_and_scales, start=start, smooth=smooth)
     current = cast(str, getenv("PATH"))
     bin_ = Path.cwd().joinpath("bin").as_posix()
     environ["PATH"] = ":".join([current, bin_])
